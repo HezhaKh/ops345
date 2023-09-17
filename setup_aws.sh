c401e06f1dcf7e4098036bc051367cbdc59f4644
hezhakh <hezhakh.b@gmail.com>
2023-09-17 02:40:01 -0400

    Updated README and modified setup_aws.sh script

--- setup_aws.sh
+++ setup_aws.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 #Author: hkhoshnoud
-#Date: Sep 16th 2023
-#Purpose: Instance setup for OPS345 Labs
+#Date: Sep 17th 2023
+#Purpose: New Username and Hostname, New User Sudo Privileges without Password, Copy SSH Key to New User.
 #Usage: ./setup_aws.sh
 
 # Ensure the user is running this script with sudo privileges
@@ -14,6 +15,12 @@ fi
 read -p "Enter the new username: " NEW_USER
 read -p "Enter the desired hostname: " NEW_HOSTNAME
 
+# Ask user for sudoers filename or use the default
+read -p "Enter the filename to store sudoers configuration (default: 10-ops345-users): " SUDOERS_FILE
+if [[ -z "$SUDOERS_FILE" ]]; then
+    SUDOERS_FILE="10-ops345-users"
+fi
+
 # Get the current username (the user you logged in as)
 CURRENT_USER=$(logname)
 
@@ -21,10 +28,10 @@ CURRENT_USER=$(logname)
 useradd $NEW_USER
 
 # Grant the new user sudo permissions without password
-echo "$NEW_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/10-ops345-users
+echo "$NEW_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/$SUDOERS_FILE
 
 # Set the right permissions for the sudoers file
-chmod 0440 /etc/sudoers.d/10-ops345-users
+chmod 0440 /etc/sudoers.d/$SUDOERS_FILE
 
 # Copy SSH key to the new user
 rsync -a /home/$CURRENT_USER/.ssh/ /home/$NEW_USER/.ssh/
@@ -34,3 +41,4 @@ chown -R $NEW_USER:$NEW_USER /home/$NEW_USER/.ssh/
 hostnamectl set-hostname $NEW_HOSTNAME
 
 echo "User and hostname set. Please test by logging in as $NEW_USER and delete it if necessary."
+
